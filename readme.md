e34b9e8a341508638a44c52dc60335a725076830
prithvijitguha <prithvijit_guha2@hotmail.com>
2023-07-24 17:00:29 +0530

    added readme

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Spark Structured Streaming Example
+
+An example repo of using Spark Structured Streaming for Ingestion Workloads
+
+Medium Article: https://medium.com/@prithvijit.guha245/pyspark-structured-streaming-for-multi-format-scalable-data-ingestion-workloads-9cdac4b7c912
--- structured_streaming_example.py
+++ structured_streaming_example.py
@@ -1,14 +1,18 @@
 # Databricks notebook source
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession 
 
 # You don't need to define the SparkSession entry point if you're on Databricks,
 # however its good to keep your code compatible
 # with multiple Spark ecosystems
-spark = SparkSession.builder.getOrCreate()
+spark = SparkSession.builder.getOrCreate() 
 
 print(spark.version)
 
-!python --version
+# COMMAND ----------
+
+# MAGIC %sh 
+# MAGIC
+# MAGIC python --version
 
 # COMMAND ----------
 
@@ -21,7 +25,7 @@ sample_dataset_schema = StructType([\
     StructField("Lastname",StringType(),True),\
     StructField("Jedi_Rank", StringType(), True),\
     StructField("IsCouncilMember", BooleanType(), True),\
-    StructField("snapshot_date", StringType(), True)
+    StructField("snapshot_date", StringType(), True) 
 ])
 
 # COMMAND ----------
@@ -39,23 +43,23 @@ dbutils.fs.mkdirs("dbfs:/FileStore/source_directory/") # make target directory f
 
 from pyspark.sql.functions import input_file_name, col, current_timestamp
 
-# we can add some other parameters like renaming the columns to a more standardized snake_case format
-# and adding input file name and record load timestamp
+# we can add some other parameters like renaming the columns to a more standardized snake_case format 
+# and adding input file name and record load timestamp 
 df_standardized = df_sink.select(
     col("Firstname").alias("first_name"),
     col("Lastname").alias("last_name"),
     col("Jedi_Rank").alias("jedi_rank"),
     col("IsCouncilMember").alias("is_council_member"),
     col("snapshot_date").cast("date"),
-    input_file_name().alias("source_file_name"),
-    current_timestamp().alias("record_load_timestamp"),
+    input_file_name().alias("source_file_name"), 
+    current_timestamp().alias("record_load_timestamp"), 
 )
 
 # COMMAND ----------
 
 
 
-# Now we an write the stream to
+# Now we an write the stream to 
 # a target location and specify a checkpoint as well
 df_standardized.writeStream\
   .format("parquet")\
@@ -73,8 +77,22 @@ display(spark.read.table("hrz_sample_dataset"))
 
 # COMMAND ----------
 
+# Incase you want to use a continuous stream, which is perpetually on
+# Default trigger (runs micro-batch as soon as it can)
+df_standardized.writeStream\
+  .format("console") \
+  .start()
+
+# ProcessingTime trigger with two-seconds micro-batch interval
+df_standardized.writeStream \
+  .format("console") \
+  .trigger(processingTime='2 seconds') \
+  .start()
+
+# COMMAND ----------
+
 # Alternate forms of reading data, read from table
-# In this case we only update the pipeline the table is updated
+# In this case we only update the pipeline the table is updated 
 df_sink_table_format = spark.readStream.table("hrz_sample_dataset")
 
 # COMMAND ----------
@@ -134,30 +152,30 @@ class IngestionFlow(BaseFlow):
             StructField("Lastname",StringType(),True),\
             StructField("Jedi_Rank", StringType(), True),\
             StructField("IsCouncilMember", BooleanType(), True),\
-            StructField("snapshot_date", StringType(), True)
+            StructField("snapshot_date", StringType(), True) 
         ])
 
         # Read the dataframe into a stream specifying the schema and load data directory
         self.df_sink = spark.readStream.format("csv")\
             .load("dbfs:/FileStore/source_directory/*.csv", schema=sample_dataset_schema, header=True)
 
-
+        
 
     @step(next=["write_datastream"])
     def standardize_data(self):
         """
-        This method we standardize the data a little bit with some column renaming
-        """
-        # we can add some other parameters like renaming the columns to a more standardized snake_case format
-        # and adding input file name and record load timestamp
+        This method we standardize the data a little bit with some column renaming 
+        """        
+        # we can add some other parameters like renaming the columns to a more standardized snake_case format 
+        # and adding input file name and record load timestamp 
         self.df_standardized = self.df_sink.select(
             col("Firstname").alias("first_name"),
             col("Lastname").alias("last_name"),
             col("Jedi_Rank").alias("jedi_rank"),
             col("IsCouncilMember").alias("is_council_member"),
             col("snapshot_date").cast("date"),
-            input_file_name().alias("source_file_name"),
-            current_timestamp().alias("record_load_timestamp"),
+            input_file_name().alias("source_file_name"), 
+            current_timestamp().alias("record_load_timestamp"), 
         )
 
     @end
@@ -166,7 +184,7 @@ class IngestionFlow(BaseFlow):
         """
         Here we write the dataframe to final DBFS/HDFS location
         """
-        # Now we an write the stream to
+        # Now we an write the stream to 
         # a target location and specify a checkpoint as well
         self.df_standardized.writeStream\
         .format("parquet")\
@@ -178,7 +196,7 @@ class IngestionFlow(BaseFlow):
 
 
 
-ingestion_flow = IngestionFlow() # create instance of pipeline
+ingestion_flow = IngestionFlow() # create instance of pipeline 
 ingestion_flow.display() # display your flow without running it
 
 # COMMAND ----------
@@ -221,22 +239,22 @@ class IngestionPipeline:
         self.spark = SparkSession.builder.getOrCreate()
 
     def read_data_stream(
-        self,
-        file_path: str,
-        file_type: str,
+        self, 
+        file_path: str, 
+        file_type: str, 
         **kwargs
     ):
         """Method to read data into a stream using Spark Structured Streaming
 
-        Kwargs is used so we can pass any others arguments that may be required.
+        Kwargs is used so we can pass any others arguments that may be required. 
         Since this method can be overriden, users
         can override this to add any other stream capabilities that may not be covered
 
         Args:
             - file_path(str): A path or directory to look for, this argument accepts glob patterns.
              eg. "dbfs:/some_directory_name/*.csv".
-            - file_type(str): A type of data format to read or look
-            for eg. "json", "parquet", "delta",
+            - file_type(str): A type of data format to read or look 
+            for eg. "json", "parquet", "delta", 
             "csv"
             - kwargs(dict): Any additional arguments to be added. eg. header, schema, etc
         """
@@ -251,24 +269,24 @@ class IngestionPipeline:
     ):
         """Method to write structured streaming dataframe to dbfs/hdfs
 
-        We create a reusable method to avoid writing boilerplate code again and again.
+        We create a reusable method to avoid writing boilerplate code again and again. 
         This method does not require a checkpoint to be
         mentioned, but it instead will create a checkpoint for you.
-        This method can be overriden, users can override this to add
+        This method can be overriden, users can override this to add 
         any writing functionality that might not be covered.
 
         Args:
-            - data_format(str): Final write format to write in
+            - data_format(str): Final write format to write in 
             eg. console, parquet, delta
             - table_name(str): A name for SparkSQL/Hive table to save as
-            - output_mode(str, optional): Output mode, options are append,
+            - output_mode(str, optional): Output mode, options are append, 
             update or complete(meaning overwrite)
-            - checkpoint_location(str, optional): An optional argument for
+            - checkpoint_location(str, optional): An optional argument for 
             checkpoint location. If not specified, then this
             method will use a default location for checkpoint location.
 
         Note:
-            Although 'checkpoint_location' is optional, if not specified a default location will be
+            Although 'checkpoint_location' is optional, if not specified a default location will be 
             used, checkpoint is required
             for tracking progress
         """
@@ -283,13 +301,15 @@ class IngestionPipeline:
         )
 
 
-
 # COMMAND ----------
 
-# Handling history data so that it does break your pipeline
+# Handling history data so that it does break your pipeline 
 # Read the dataframe into a stream specifying the schema and load data directory
 # We also mention the max files per trigger so that we only 20 per trigger pull
 df_sink = spark.readStream.format("csv")\
     .option("maxFilesPerTrigger", 20)\
     .load("dbfs:/FileStore/source_directory/*.csv", schema=sample_dataset_schema, header=True)
+
+# COMMAND ----------
+
 
